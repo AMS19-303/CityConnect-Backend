7256831cfdc0482d3a93d394ab8375de9cb92e9e
Eurico Dias <dias.eurico@ua.pt>
2019-12-13 20:28:34 +0000

    Correct v2

--- api.py
+++ api.py
@@ -106,7 +106,7 @@ def order():
 
         lst_items = data['items']
 
-        cur.execute("INSERT INTO public.order (user_id, active, total_price, timestamp, address) VALUES (%s, %s, %s, '%s', %s)" % (uid, active, total_price, delivery_date, address))
+        cur.execute("INSERT INTO public.order (user_id, active, total_price, timestamp, address) VALUES (%s, %s, %s, '%s', '%s')" % (uid, active, total_price, delivery_date, address))
         oid = cur.lastrowid
 
         for item in lst_items:
@@ -121,7 +121,7 @@ def order():
             else:
                 req = item['name']
                 cur.execute("INSERT INTO order_item (quantity, cumul_price, discount, product_id, order_id, request)"
-                            + " VALUES (%s, %s, %s, %s, %s)" % ('0', '0', '0', None, oid, req))
+                            + " VALUES (%s, %s, %s, %s, '%s')" % ('0', '0', '0', None, oid, req))
 
 
 @app.route("/profile")
